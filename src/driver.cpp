401c53959bf72de4790510dd3665866817c1278a
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-06-07 18:07:50 +0200

    Start code for client

--- src/driver.cpp
+++ src/driver.cpp
@@ -3,6 +3,10 @@
 #include <cstdio>
 #include <cstring>
 
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
 extern "C" {
 #include <lirc/lirc_client.h>
 }
@@ -28,12 +32,41 @@ int main(){
     //Initiate LIRC. Exit on failure
     char lirc_name[] = "lirc";
     if(lirc_init(lirc_name, 1) == -1){
+        std::cout << "asgard:ir: Failed to init LIRC" << std::endl;
         return 1;
     }
 
-    struct lirc_config* config;
+    //Open the socket
+    auto socket_fd = socket(PF_UNIX, SOCK_STREAM, 0);
+    if(socket_fd < 0){
+        std::cout << "asgard:ir: socket() failed" << std::endl;
+        return 1;
+    }
 
-    //Read the default LIRC config at /etc/lirc/lircd.conf  This is the config for your remote.
+    //Init the address
+    struct sockaddr_un address;
+    memset(&address, 0, sizeof(struct sockaddr_un));
+    address.sun_family = AF_UNIX;
+    snprintf(address.sun_path, UNIX_PATH_MAX, "/tmp/asgard_socket");
+
+    //Connect to the server
+    if(connect(socket_fd, (struct sockaddr *) &address, sizeof(struct sockaddr_un)) != 0){
+        std::cout << "asgard:ir: connect() failed" << std::endl;
+        return 1;
+    }
+
+    char buffer[4096];
+
+    auto nbytes = snprintf(buffer, 4096, "hello from a client");
+    write(socket_fd, buffer, nbytes);
+
+    nbytes = read(socket_fd, buffer, 4096);
+    buffer[nbytes] = 0;
+
+    printf("MESSAGE FROM SERVER: %s\n", buffer);
+
+    //Read the default LIRC config
+    struct lirc_config* config;
     if(lirc_readconfig(NULL,&config,NULL)==0){
         char* code;
 
@@ -51,10 +84,15 @@ int main(){
 
         //Frees the data structures associated with config.
         lirc_freeconfig(config);
+    } else {
+        std::cout << "asgard:ir: Failed to read LIRC config" << std::endl;
     }
 
     //Closes LIRC
     lirc_deinit();
+ 
+    //Close the socket
+    close(socket_fd);
 
     return 0;
 }
